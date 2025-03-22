4a6b1492b67751ed996a5a488934013bc613be1b
samuel <samupalo3@gmail.com>
2025-03-22 11:32:13 -0500

    Model Security

---
+++ Entity/Model/Modulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Modulo
+    {
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public string Activate { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
---
+++ Entity/Model/Permission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+
+        public string Can_Read { get; set; }
+        public string Can_Create { get; set;}
+        public string Can_Update { get; set;}
+        public string Can_Delete { get; set;}
+
+        public DateTime CreateAt { get; set; }
+    }
+}
